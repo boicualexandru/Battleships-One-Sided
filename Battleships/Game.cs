c856203beb627028e18a1f7e7f277c47bcbdc13a
Alex Boicu <boicualexandru@mail.com>
2019-08-22 21:59:34 +0300

    Game class responsible for UI

---
+++ Battleships/Game.cs
@@ -0,0 +1,110 @@
+﻿using Battleships.Models;
+using System;
+
+namespace Battleships
+{
+    public class Game : IGame
+    {
+        private readonly IMap _map;
+
+        public Game(IMap map)
+        {
+            _map = map;
+        }
+
+        public void Start()
+        {
+            _map.Load();
+
+            while (!_map.IsFinished)
+            {
+                Loop();
+            }
+
+            DrawMap();
+
+            Console.WriteLine("\n\nCongratulations, you won!");
+        }
+
+        private void Loop()
+        {
+            try
+            {
+                DrawMap();
+                var location = ReadCoordinates();
+
+                _map.TryHit(location);
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine("\n" + ex.Message);
+                Console.WriteLine("Press any key to repeat the last move...");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nSomething went wrong.");
+                Console.WriteLine("Press any key to repeat the last move...");
+                Console.ReadKey();
+            }
+        }
+
+        private void DrawMap()
+        {
+            Console.Clear();
+            Console.Write(_map.ToString());
+        }
+
+        private Location ReadCoordinates()
+        {
+            var x = ReadX();
+            var y = ReadY();
+
+            return new Location
+            {
+                X = x,
+                Y = y
+            };
+        }
+
+        private int ReadX()
+        {
+            Console.WriteLine($"\n\nChose X (A-{(char)('A' + _map.Width - 1)}):");
+            var xString = Console.ReadLine();
+            xString = xString.Trim().ToLower();
+
+            var x = xString[0] - 'a';
+
+            while (x < 0 || x >= _map.Width)
+            {
+                Console.WriteLine("X coordinate is out of range. Please enter the value again.");
+                xString = Console.ReadLine();
+                xString = xString.Trim().ToLower();
+
+                x = xString[0] - 'a';
+            }
+
+            return x;
+        }
+
+        private int ReadY()
+        {
+            Console.WriteLine($"\n\nChose Y (1-{_map.Height}):");
+            var yString = Console.ReadLine();
+            yString = yString.Trim().ToLower();
+
+            var y = int.Parse(yString) - 1;
+
+            while (y < 0 || y >= _map.Height)
+            {
+                Console.WriteLine("Y coordinate is out of range. Please enter the value again.");
+                yString = Console.ReadLine();
+                yString = yString.Trim().ToLower();
+
+                y = int.Parse(yString) - 1;
+            }
+
+            return y;
+        }
+    }
+}
--- Battleships/Program.cs
+++ Battleships/Program.cs
@@ -19,90 +19,17 @@ namespace Battleships
                 .AddTransient<IBattleShipFactory, BattleShipFactory>()
                 .AddTransient<IDestroyerShipFactory, DestroyerShipFactory>()
                 .AddTransient<IMap, Map>()
+                .AddTransient<IGame, Game>()
                 .AddSingleton<MapConfiguration>(_ => {
                     return config.GetSection("MapConfiguration").Get<MapConfiguration>();
                 })
                 .BuildServiceProvider();
 
-            var map = serviceProvider.GetService<IMap>();
-            map.Load();
+            var game = serviceProvider.GetService<IGame>();
+            game.Start();
 
-            while (!map.IsFinished)
-            {
-                try
-                {
-                    Console.Clear();
-                    Console.Write(map.ToString());
-
-                    var x = ReadX(map.Width);
-                    var y = ReadY(map.Height);
-
-                    map.TryHit(new Location
-                    {
-                        X = x,
-                        Y = y
-                    });
-                }
-                catch(InvalidOperationException ex)
-                {
-                    Console.WriteLine("\n" + ex.Message);
-                    Console.WriteLine("Press any key to repeat the last move...");
-                    Console.ReadKey();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("\nSomething went wrong.");
-                    Console.WriteLine("Press any key to repeat the last move...");
-                    Console.ReadKey();
-                }
-            }
-
-            Console.Clear();
-            Console.Write(map.ToString());
-
-            Console.WriteLine("\n\nCongratulations, you won!");
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
-        }
-
-        private static int ReadX(int mapWidth)
-        {
-            Console.WriteLine($"\n\nChose X (A-{(char)('A' + mapWidth - 1)}):");
-            var xString = Console.ReadLine();
-            xString = xString.Trim().ToLower();
-
-            var x = xString[0] - 'a';
-
-            while (x < 0 || x >= mapWidth)
-            {
-                Console.WriteLine("X coordinate is out of range. Please enter the value again.");
-                xString = Console.ReadLine();
-                xString = xString.Trim().ToLower();
-
-                x = xString[0] - 'a';
-            }
-
-            return x;
-        }
-
-        private static int ReadY(int mapHeight)
-        {
-            Console.WriteLine($"\n\nChose Y (1-{mapHeight}):");
-            var yString = Console.ReadLine();
-            yString = yString.Trim().ToLower();
-
-            var y = int.Parse(yString) - 1;
-
-            while (y < 0 || y >= mapHeight)
-            {
-                Console.WriteLine("Y coordinate is out of range. Please enter the value again.");
-                yString = Console.ReadLine();
-                yString = yString.Trim().ToLower();
-
-                y = int.Parse(yString) - 1;
-            }
-
-            return y;
         }
     }
 }
