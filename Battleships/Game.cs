5841cc3780e363303625cd1b1e9ace8b3b0b4713
Alex Boicu <boicualexandru@mail.com>
2019-08-23 16:28:01 +0300

    Add UserInterface class

--- Battleships/Game.cs
+++ Battleships/Game.cs
@@ -6,10 +6,12 @@ namespace Battleships
     public class Game : IGame
     {
         private readonly IMap _map;
+        private readonly IUserInterface _userInterface;
 
-        public Game(IMap map)
+        public Game(IMap map, IUserInterface userInterface)
         {
             _map = map;
+            _userInterface = userInterface;
         }
 
         public void Start()
@@ -23,7 +25,7 @@ namespace Battleships
 
             DrawMap();
 
-            Console.WriteLine("\n\nCongratulations, you won!");
+            _userInterface.Display("\n\n\nCongratulations, you won!");
         }
 
         private void Loop()
@@ -37,22 +39,22 @@ namespace Battleships
             }
             catch (InvalidOperationException ex)
             {
-                Console.WriteLine("\n" + ex.Message);
-                Console.WriteLine("Press any key to repeat the last move...");
-                Console.ReadKey();
+                _userInterface.Display("\n\n" + ex.Message);
+                _userInterface.Display("\nPress Enter to repeat the last move...");
+                _userInterface.GetInput();
             }
             catch (Exception ex)
             {
-                Console.WriteLine("\nSomething went wrong.");
-                Console.WriteLine("Press any key to repeat the last move...");
-                Console.ReadKey();
+                _userInterface.Display("\n\nSomething went wrong.");
+                _userInterface.Display("\nPress Enter key to repeat the last move...");
+                _userInterface.GetInput();
             }
         }
 
         private void DrawMap()
         {
-            Console.Clear();
-            Console.Write(_map.ToString());
+            _userInterface.Clear();
+            _userInterface.Display(_map.ToString());
         }
 
         private Location ReadCoordinates()
@@ -69,16 +71,16 @@ namespace Battleships
 
         private int ReadX()
         {
-            Console.WriteLine($"\n\nChose X (A-{(char)('A' + _map.Width - 1)}):");
-            var xString = Console.ReadLine();
+            _userInterface.Display($"\n\n\nChose X (A-{(char)('A' + _map.Width - 1)}):");
+            var xString = _userInterface.GetInput();
             xString = xString.Trim().ToLower();
 
             var x = xString[0] - 'a';
 
             while (x < 0 || x >= _map.Width)
             {
-                Console.WriteLine("X coordinate is out of range. Please enter the value again.");
-                xString = Console.ReadLine();
+                _userInterface.Display("\nX coordinate is out of range. Please enter the value again.");
+                xString = _userInterface.GetInput();
                 xString = xString.Trim().ToLower();
 
                 x = xString[0] - 'a';
@@ -89,16 +91,16 @@ namespace Battleships
 
         private int ReadY()
         {
-            Console.WriteLine($"\n\nChose Y (1-{_map.Height}):");
-            var yString = Console.ReadLine();
+            _userInterface.Display($"\n\n\nChose Y (1-{_map.Height}):");
+            var yString = _userInterface.GetInput();
             yString = yString.Trim().ToLower();
 
             var y = int.Parse(yString) - 1;
 
             while (y < 0 || y >= _map.Height)
             {
-                Console.WriteLine("Y coordinate is out of range. Please enter the value again.");
-                yString = Console.ReadLine();
+                _userInterface.Display("\nY coordinate is out of range. Please enter the value again.");
+                yString = _userInterface.GetInput();
                 yString = yString.Trim().ToLower();
 
                 y = int.Parse(yString) - 1;
--- Battleships/Program.cs
+++ Battleships/Program.cs
@@ -19,6 +19,7 @@ namespace Battleships
                 .AddTransient<IBattleShipFactory, BattleShipFactory>()
                 .AddTransient<IDestroyerShipFactory, DestroyerShipFactory>()
                 .AddTransient<IMap, Map>()
+                .AddTransient<IUserInterface, UserInterface>()
                 .AddTransient<IGame, Game>()
                 .AddSingleton<MapConfiguration>(_ => {
                     return config.GetSection("MapConfiguration").Get<MapConfiguration>();
